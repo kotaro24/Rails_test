9d6b24e14bae21b8a8a84b672719db2f6b69ab93
”kotaro.T” <>
2020-11-19 14:19:50 +0000

    Heroku

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,6 +19,10 @@ class SessionsController < ApplicationController
    end
 
   private
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end 
+  
   def log_in(user)
     session[:user_id] = user.id
   end
@@ -28,9 +32,7 @@ class SessionsController < ApplicationController
     @current_user = nil
   end
   
-  def session_params
-    params.require(:session).permit(:email, :password)
-  end 
+
 
   
 end
